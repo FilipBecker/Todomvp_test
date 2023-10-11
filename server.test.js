8984da4cba6301d4ebf66a10de45e66fd14b59bd
herkommer <herkommer@users.noreply.github.com>
2023-10-11 08:55:03 +0200

    Added Server Alive Test for /ping route

---
+++ server.test.js
@@ -0,0 +1,16 @@
+//Vi behöver inte testa servern när den är igång, 
+//eftersom vi använder supertest för att
+//simulera att servern är igång.
+
+//Om vi vill kan vi börja med att bara
+//prova att servern åtminstone existerar
+//och svarar med 200 och en pong på vårt ping
+
+const server = require('./server');
+const request = require('supertest');
+
+test('Server is alive', async () => {
+    const response = await request(server).get('/ping');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('pong');
+});
